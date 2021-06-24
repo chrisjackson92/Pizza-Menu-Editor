088f724b2168666652860743d14385e3c5ce9d97
chrisjackson92 <67966281+chrisjackson92@users.noreply.github.com>
2021-06-24 16:24:15 -0400

    First commit

---
+++ PizzaOrderExceptionHandling/PizzaOrder.cs
@@ -0,0 +1,77 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace PizzaOrderExceptionHandling
+{
+    class PizzaOrder
+    {
+        int size;
+        string toppings;
+        decimal price;
+
+        public PizzaOrder()
+        {
+            size = 12;
+            toppings = "cheese";
+            price = 8m;
+        }
+
+        public PizzaOrder(int tempSize, string tempToppings, decimal tempPrice)
+        {
+            Size = tempSize;
+            toppings = tempToppings;
+            Price = tempPrice;
+        }
+
+        public int Size
+        {
+            get
+            {
+                return size;
+            }
+            set
+            {
+                if (value >= 12 & value <= 16)
+                {
+                    size = value;
+                }
+                else
+                {
+                    //throw new FormatException("Size must be entered as an integer.");
+                    throw new Exception("Size must be between 12 and 16.");
+                }
+            }
+        }
+
+        public decimal Price
+        {
+            get
+            {
+                return price;
+            }
+            set
+            {
+                if (value >= 0)
+                {
+                    price = value;
+                }
+                else
+                {
+                    //throw new FormatException("Price must be enterted in a number format.");
+                    throw new Exception("Price must be a positive number.");
+                }
+            }
+        }
+
+        public string PizzaInfo()
+        {
+            string info = $"Size:     {size}\n" +
+                          $"Toppings: {toppings}\n" +
+                          $"Price:    {price:C}\n";
+            return info;
+        }
+    }
+}
---
+++ PizzaOrderExceptionHandling/Program.cs
@@ -0,0 +1,125 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace PizzaOrderExceptionHandling
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            PizzaOrder[] pizzaArray = new PizzaOrder[4];
+            pizzaArray[0] = new PizzaOrder();
+            pizzaArray[1] = new PizzaOrder(13, "pepperoni", 13);
+            pizzaArray[2] = new PizzaOrder(14, "Veggie Style", 14);
+            pizzaArray[3] = new PizzaOrder(15, "Meat Lovers", 15);
+
+            int userChoice = 0, pizzaNumber = 0, newSize;
+            decimal newPrice = 0m;
+
+            string keepGoing = "y";
+            while (keepGoing == "y")
+            {
+                Console.WriteLine("-----Menu-----");
+                Console.WriteLine("1. Change Price");
+                Console.WriteLine("2. Change Size");
+
+                Console.WriteLine("");
+                Console.WriteLine("Please enter the number of the command:");
+                userChoice = int.Parse(Console.ReadLine());
+
+                if(userChoice == 1)
+                {
+                    try
+                    {
+                        Console.WriteLine("");
+                        for (int i = 0; i < 4; i++)
+                        {
+                            Console.WriteLine($"Pizza Number: {i}");
+                            Console.WriteLine(pizzaArray[i].PizzaInfo());
+                            Console.WriteLine("");
+                        }
+                        Console.WriteLine("Please enter the pizza number you want to edit");
+                        pizzaNumber = int.Parse(Console.ReadLine());
+
+                        Console.WriteLine("");
+
+                        Console.WriteLine("Please enter the new price of the pizza:");
+                        newPrice = decimal.Parse(Console.ReadLine());
+
+                        pizzaArray[pizzaNumber].Price = newPrice;
+
+                        Console.WriteLine("");
+
+                        for (int i = 0; i < 4; i++)
+                        {
+                            Console.WriteLine($"Pizza Number: {i}");
+                            Console.WriteLine(pizzaArray[i].PizzaInfo());
+                            Console.WriteLine("");
+                        }
+                    }
+                    catch(FormatException formatError)
+                    {
+                        Console.WriteLine("Please enter the price in a decimal format.");
+                    }
+                    catch(IndexOutOfRangeException indexError)
+                    {
+                        Console.WriteLine("Please enter a pizza number within the list.");
+                    }
+                    catch(Exception e)
+                    {
+                        Console.WriteLine(e.Message);
+                    }
+                }
+                else if(userChoice == 2)
+                {
+                    try
+                    {
+                        Console.WriteLine("");
+                        for (int i = 0; i < 4; i++)
+                        {
+                            Console.WriteLine($"Pizza Number: {i}");
+                            Console.WriteLine(pizzaArray[i].PizzaInfo());
+                            Console.WriteLine("");
+                        }
+                        Console.WriteLine("Please enter the pizza number you want to edit");
+                        pizzaNumber = int.Parse(Console.ReadLine());
+
+                        Console.WriteLine("");
+
+                        Console.WriteLine("Please enter the new size of the pizza:");
+                        newSize = int.Parse(Console.ReadLine());
+
+                        pizzaArray[pizzaNumber].Size = newSize;
+
+                        Console.WriteLine("");
+
+                        for (int i = 0; i < 4; i++)
+                        {
+                            Console.WriteLine($"Pizza Number: {i}");
+                            Console.WriteLine(pizzaArray[i].PizzaInfo());
+                            Console.WriteLine("");
+                        }
+                    }
+                    catch (FormatException formatError)
+                    {
+                        Console.WriteLine("Please enter the size as a integer.");
+                    }
+                    catch (IndexOutOfRangeException indexError)
+                    {
+                        Console.WriteLine("Please enter a pizza number within the list.");
+                    }
+                    catch (Exception e)
+                    {
+                        Console.WriteLine(e.Message);
+                    }
+                }
+
+                Console.WriteLine("Do you wish to continue? (Enter 'y' for yes)");
+                keepGoing = Console.ReadLine();
+            }
+        }
+    }
+}
